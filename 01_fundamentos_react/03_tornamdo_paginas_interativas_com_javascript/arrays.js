d3a04a18719789c858c31c7cc222b466e1b1d488
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-20 21:39:42 -0300

    Arrays

---
+++ 01_fundamentos_react/03_tornamdo_paginas_interativas_com_javascript/arrays.js
@@ -0,0 +1,17 @@
+const alunos = [
+    {
+        nome: 'Pablo',
+        idade: 25
+    },
+    {
+        nome: 'José',
+        idade: 19
+    },
+]
+
+alunos.push({
+    nome: 'João',
+    idade: 35
+})
+
+console.log(alunos[2].idade)
