1400be73d3bb25937e586292801a23976a62e46b
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-26 20:58:47 -0300

    finish config esling prettier and editorconfig

--- 02_conceitos_basicos_biblioteca/01_criando_projeto_react_do_zero/src/App.js
+++ 02_conceitos_basicos_biblioteca/01_criando_projeto_react_do_zero/src/App.js
@@ -1,35 +1,38 @@
-import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from 'react';
 
-const App = () => {
+function App() {
+  const [count, setCount] = useState(0);
+  const [usuario, setUsuario] = useState('');
+  const [usuarios, setUsuarios] = useState(['Pablo', 'José', 'Manoel']);
 
-    const [count, setCount] = useState(0)
-    const [usuario, setUsuario] = useState("")
-    const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"]);
+  const handleAddUser = () => {
+    setUsuarios([...usuarios, usuario]);
+    setUsuario('');
+  };
 
-    const handleAddUser = () =>{
-        setUsuarios([...usuarios, usuario])
-        setUsuario("")
-    }
+  useEffect(() => {
+    setCount(usuarios.length);
+  }, [usuarios]);
 
-    useEffect(() =>{
-        setCount(usuarios.length)
-    }, [usuarios])
-
-    return (
-        <div className="App">
-            <h1> Hello DIO!</h1>
-            <h3> Total {count}</h3>
-            <div>
-                <input value={usuario} onChange={(event) => setUsuario(event.target.value)}
-                      />
-                <button onClick={handleAddUser}>Adicionar</button>
-            </div>
-            <hr/>
-            {usuarios.map((item) => (
-                <p key={item}>{item}</p>
-            ))}
-        </div>
-    );
-};
+  return (
+    <div className='App'>
+      <h1> Hello DIO!</h1>
+      <h3>
+        Total
+        {count}
+      </h3>
+      <div>
+        <input value={usuario} onChange={(event) => setUsuario(event.target.value)} />
+        <button type='submit' onClick={handleAddUser}>
+          Adicionar
+        </button>
+      </div>
+      <hr />
+      {usuarios.map((item) => (
+        <p key={item}>{item}</p>
+      ))}
+    </div>
+  );
+}
 
 export default App;
