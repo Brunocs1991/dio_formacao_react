e14ab4dbbfb34453b2dd118ba3b5eb944946b041
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-26 19:19:45 -0300

    Conceitos fundamentais para react, ciclo de vida e jsc

--- 02_conceitos_basicos_biblioteca/01_criando_projeto_react_do_zero/src/App.js
+++ 02_conceitos_basicos_biblioteca/01_criando_projeto_react_do_zero/src/App.js
@@ -1,18 +1,31 @@
 import React, {useEffect, useState} from "react";
 
 const App = () => {
+
+    const [count, setCount] = useState(0)
+    const [usuario, setUsuario] = useState("")
     const [usuarios, setUsuarios] = useState(["Pablo", "José", "Manoel"]);
 
-    useEffect(()=>{
-        setTimeout(()=>{
-            setUsuarios([...usuarios, "Bruno"])
-        },3000)
-    }, [])
+    const handleAddUser = () =>{
+        setUsuarios([...usuarios, usuario])
+        setUsuario("")
+    }
+
+    useEffect(() =>{
+        setCount(usuarios.length)
+    }, [usuarios])
 
     return (
         <div className="App">
             <h1> Hello DIO!</h1>
-            {usuarios.map((item) =>(
+            <h3> Total {count}</h3>
+            <div>
+                <input value={usuario} onChange={(event) => setUsuario(event.target.value)}
+                      />
+                <button onClick={handleAddUser}>Adicionar</button>
+            </div>
+            <hr/>
+            {usuarios.map((item) => (
                 <p key={item}>{item}</p>
             ))}
         </div>
