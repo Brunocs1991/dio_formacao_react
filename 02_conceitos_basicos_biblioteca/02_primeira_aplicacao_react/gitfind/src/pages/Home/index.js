117b18e99aae0aad01144bc17a066db24a97fac9
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-27 15:22:48 -0300

    Background and button

--- 02_conceitos_basicos_biblioteca/02_primeira_aplicacao_react/gitfind/src/pages/Home/index.js
+++ 02_conceitos_basicos_biblioteca/02_primeira_aplicacao_react/gitfind/src/pages/Home/index.js
@@ -1,9 +1,20 @@
 import Header from "../../components/Header";
+import background from '../../assets/background.png'
+import './styles.css'
 
 const Index = () => {
     return (
         <div className="App">
             <Header/>
+            <div className="conteudo">
+                <img src={background} className="background" alt="background app"/>
+                <div className="info">
+                    <div>
+                        <input name="usuario" placeholder="@username"/>
+                        <button type="submit">Buscar</button>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 }
