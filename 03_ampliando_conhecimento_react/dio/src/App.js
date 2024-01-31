08d3d3ded2f568120d859d61d50b01d6bf021b66
Brunocs1991 <brunocosta.1991@hotmail.com>
2024-01-31 19:07:16 -0300

    UserState

--- 03_ampliando_conhecimento_react/dio/src/App.js
+++ 03_ampliando_conhecimento_react/dio/src/App.js
@@ -4,6 +4,7 @@ import Home from "./pages/home";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Login from "./pages/login";
 import Feed from "./pages/feed";
+import {Tests} from "./pages";
 
 const App = () => {
 
@@ -13,6 +14,7 @@ const App = () => {
                 <Route path="/" element={<Home/>}/>
                 <Route path="/login" element={<Login/>}/>
                 <Route path="/feed" element={<Feed/>}/>
+                <Route path="/teste" element={<Tests/>}/>
             </Routes>
         </BrowserRouter>
     );
